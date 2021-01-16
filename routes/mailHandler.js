318c69d31c5c38f0de8c227949021276902bc01b
Myles <myles.leadbeater@gmail.com>
2021-01-16 14:30:02 +1030

    m

--- routes/mailHandler.js
+++ routes/mailHandler.js
@@ -2,48 +2,49 @@ var express = require('express');
 var router = express.Router();
 
 //send Email function
-const nodemailer = require('nodemailer');
-const mailGun = require('nodemailer-mailgun-transport');
+// const nodemailer = require('nodemailer');
+// const mailGun = require('nodemailer-mailgun-transport');
+
+const auth = require('../routes/auth.js');
 
 
-const auth = {
-    auth: {
-        api_key: '11edcb4c10e8d74fc0537e716f559c43-28d78af2-9ce8a1a6',
-        domain: 'sandboxed4c489db184488aa000d927b994b171.mailgun.org'
-    }
-}
+// const transporter = nodemailer.createTransport(mailGun(auth));
+//
+// const sendMail = (userName, email, item, msg, cb) => {
+//     const mailOptions = {
+//         userName,
+//         email,
+//         item,
+//         msg,
+//         to: 'Rarencibia@comcast.net',
+//     };
+//
+//     transporter.sendMail(mailOptions, function (err,data) {
+//         if(err) {
+//             cb(err, null);
+//         } else{
+//             cb(null, data);
+//         }
+//     });
+//
+// };
 
-const transporter = nodemailer.createTransport(mailGun(auth));
-
-const sendMail = (userName, email, item, msg, cb) => {
-    const mailOptions = {
-        userName,
-        email,
-        item,
-        msg,
-        to: 'Rarencibia@comcast.net',
-    };
-
-    transporter.sendMail(mailOptions, function (err,data) {
-        if(err) {
-            cb(err, null);
-        } else{
-            cb(null, data);
-        }
-    });
-
-};
-
+router.get('/', (req, res) => {
+    res.send("{}");
+});
 router.post('/', (req, res) => {
-    const { subject, email, text } = req.body;
-    console.log('data: ', req.body);
-    sendMail(userName, email, item, msg, function (err, data) {
-      if(err) {
-        res.status(500).json({ message: 'Internal Error'});
-      }else {
-        res.json({ message: 'Email Sent! You will hear back from us shortly!'});
-      }
-    });
-  });
+// console.log('data: ', req.body);
+    res.send("hello");
+    // const { subject, email, text } = req.body;
+    // console.log('data: ', req.body);
+    // sendMail(userName, email, item, msg, function (err, data) {
+    //   if(err) {
+    //     res.status(500).json({ message: 'Internal Error'});
+    //     res.send("mail failed");
+    //   }else {
+    //     res.json({ message: 'Email Sent! You will hear back from us shortly!'});
+    //   }
+    // });
+});
 
-  module.exports = router;
+module.exports = router;
