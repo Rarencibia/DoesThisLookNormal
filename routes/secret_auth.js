fca92db0675cb86574e042d006e2f2138a09b288
Myles <myles.leadbeater@gmail.com>
2021-01-16 14:56:31 +1030

    m

---
+++ routes/secret_auth.js
@@ -0,0 +1,9 @@
+
+const auth = {
+    auth: {
+        api_key: '11edcb4c10e8d74fc0537e716f559c43-28d78af2-9ce8a1a6',
+        domain: 'sandboxed4c489db184488aa000d927b994b171.mailgun.org'
+    }
+}
+
+module.exports = auth;
