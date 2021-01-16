bbb5e7d2839089ada099fcf242a30b6f4c7d2092
Myles <myles.leadbeater@gmail.com>
2021-01-16 14:50:18 +1030

    m

--- routes/mailHandler.js
+++ routes/mailHandler.js
@@ -2,50 +2,55 @@ var express = require('express');
 var router = express.Router();
 
 //send Email function
-// const nodemailer = require('nodemailer');
-// const mailGun = require('nodemailer-mailgun-transport');
+const nodemailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
 
 const auth = require('../routes/auth.js');
+const transporter = nodemailer.createTransport(mailGun(auth));
 
+const sendMail = (userName, email, item, msg, cb) => {
+    const mailOptions = {
+        userName,
+        email,
+        item,
+        msg,
+        to: 'Rarencibia@comcast.net',
+    };
 
-// const transporter = nodemailer.createTransport(mailGun(auth));
-//
-// const sendMail = (userName, email, item, msg, cb) => {
-//     const mailOptions = {
-//         userName,
-//         email,
-//         item,
-//         msg,
-//         to: 'Rarencibia@comcast.net',
-//     };
-//
-//     transporter.sendMail(mailOptions, function (err,data) {
-//         if(err) {
-//             cb(err, null);
-//         } else{
-//             cb(null, data);
-//         }
-//     });
-//
-// };
+    transporter.sendMail(mailOptions, function (err,data) {
+        if(err) {
+            cb(err, null);
+        } else{
+            cb(null, data);
+        }
+    });
+
+};
 
 router.get('/', (req, res) => {
     res.send("{}");
 });
-router.post('/', (req, res) => {
-// console.log('data: ', req.body);
-    // res.send("hello");
-    // res.status(500).json({ message: 'test_error'});
 
-    const { subject, email, text } = req.body;
-    console.log('data: ', req.body);
-    sendMail(userName, email, item, msg, function (err, data) {
-      if(err) {
-        res.status(500).json({ message: 'Internal Error'});
-        res.send("mail failed");
-      }else {
-        res.json({ message: 'Email Sent! You will hear back from us shortly!'});
-      }
+router.post('/', (req, res) => {
+    var userName = "userName";
+    var item = "item";
+    var subject = "subject";
+    var email = "email";
+    var text = "text";
+
+    console.log('req.body: ', req.body);
+
+
+
+    res.send("hello");
+    res.status(500).json({ message: 'test_error'});
+
+    sendMail(userName, email, item, text, function (err, data) {
+        if(err) {
+            res.status(500).json({ message: 'Internal Error'});
+        }else {
+            res.json({ message: 'Email Sent! You will hear back from us shortly!'});
+        }
     });
 });
 
