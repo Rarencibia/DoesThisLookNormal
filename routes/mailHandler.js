1f67ce00b711cd8e5ec7eb67a467033f85bcb100
Myles <myles.leadbeater@gmail.com>
2021-01-16 14:10:55 +1030

    m

--- routes/mailHandler.js
+++ routes/mailHandler.js
@@ -34,22 +34,16 @@ const sendMail = (userName, email, item, msg, cb) => {
 
 };
 
-
-
-
-router.post('/', function(req, res, next) {
-    res.send('{}');
+app.post('/', (req, res) => {
+    const { subject, email, text } = req.body;
+    console.log('data: ', req.body);
+    sendMail(userName, email, item, msg, function (err, data) {
+      if(err) {
+        res.status(500).json({ message: 'Internal Error'});
+      }else {
+        res.json({ message: 'Email Sent! You will hear back from us shortly!'});
+      }
+    });
   });
-// app.post('/email', (req, res) => {
-//     const { subject, email, text } = req.body;
-//     console.log('data: ', req.body);
-//     sendMail(userName, email, item, msg, function (err, data) {
-//       if(err) {
-//         res.status(500).json({ message: 'Internal Error'});
-//       }else {
-//         res.json({ message: 'Email Sent! You will hear back from us shortly!'});
-//       }
-//     });
-//   });
 
   module.exports = router;
