a1f2f624fd6377cd8067d1aec4c22388b602e2b3
Rarencibia <40923510+Rarencibia@users.noreply.github.com>
2021-01-15 22:38:16 -0500

    bug fixes

--- routes/mailHandler.js
+++ routes/mailHandler.js
@@ -1,19 +1,43 @@
 var express = require('express');
 var router = express.Router();
-const sendMail = require('../public/javascripts/mail.js')
-
-
-/* GET About Us page. */
-router.get('/', function(req, res, next) {
-  res.render('shop', {});
-});
-
-
-
-
 
 //send Email function
-router.get('/', function(req, res, next) {
+const nodemailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
+
+
+const auth = {
+    auth: {
+        api_key: '11edcb4c10e8d74fc0537e716f559c43-28d78af2-9ce8a1a6',
+        domain: 'sandboxed4c489db184488aa000d927b994b171.mailgun.org'
+    }
+}
+
+const transporter = nodemailer.createTransport(mailGun(auth));
+
+const sendMail = (userName, email, item, msg, cb) => {
+    const mailOptions = {
+        userName,
+        email,
+        item,
+        msg,
+        to: 'Rarencibia@comcast.net',
+    };
+
+    transporter.sendMail(mailOptions, function (err,data) {
+        if(err) {
+            cb(err, null);
+        } else{
+            cb(null, data);
+        }
+    });
+
+};
+
+
+
+
+router.post('/', function(req, res, next) {
     res.send('{}');
   });
 // app.post('/email', (req, res) => {
