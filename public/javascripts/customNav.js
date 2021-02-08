9b678cea0f7808eda5bc179182d0a281d54fa2a5
Rarencibia <40923510+Rarencibia@users.noreply.github.com>
2021-02-07 19:41:04 -0500

    Added new nav 'custom'

---
+++ public/javascripts/customNav.js
@@ -0,0 +1,31 @@
+// let c = document.getElementById("c");
+// let u = document.getElementById("u");
+// let s = document.getElementById("s");
+// let t = document.getElementById("t");
+// let o = document.getElementById("o");
+// let m = document.getElementById("m");
+
+
+// let letterList = [];
+
+// function makeList() {
+//     letterList.push(c);
+//     letterList.push(u);
+//     letterList.push(s);
+//     letterList.push(t);
+//     letterList.push(o);
+//     letterList.push(m);
+//     console.log(letterList);
+// };
+
+// makeList();
+
+
+
+// function changeColors(item, color1,color2,color3,color4,color5,color6){
+//     for(let i = 0; i < letterList.length; i++){
+//         letterList[i].style
+//     }
+
+// };
+
